9aeb1a2c3e042a26141139e2458bada61dadd81f
catherine <vuthi.esther@gmail.com>
2024-05-17 23:49:49 +0300

    changes to image section

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-12 gap-5">
       <h1>User profiles</h1>
       <div className="h-40 w-full rounded-lg border border-white">
-        <div>image</div>
+        <div>
+          <div>Image</div>
+        </div>
 
       </div>
     </main>
