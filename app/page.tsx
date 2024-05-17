68545806ce1f62055a3de6b15125fa13bc1ddc74
catherine <vuthi.esther@gmail.com>
2024-05-17 23:53:31 +0300

    Display section in col

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12 gap-5">
       <h1>User profiles</h1>
-      <div className="h-40 w-full rounded-lg border border-white">
+      <div className=" flex flex-col h-40 w-full rounded-lg border border-white">
         <div className="flex">
           <div>Image</div>
           <div>
