58e197c8d582b6feabb0c2422ea80e39942d098c
catherine <vuthi.esther@gmail.com>
2024-05-17 23:48:57 +0300

    Image section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-12 gap-5">
       <h1>User profiles</h1>
       <div className="h-40 w-full rounded-lg border border-white">
+        <div>image</div>
 
       </div>
     </main>
