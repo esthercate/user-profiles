c2ccde4676735c151469251a678dd386326f70c4
catherine <vuthi.esther@gmail.com>
2024-05-20 22:31:52 +0300

    Add woman avatar

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ export default function Home() {
       <h1>User profiles</h1>
       <div className=" flex flex-col h-40 w-full rounded-lg border border-white">
         <div className="flex">
-          <div>Image</div>
           <div>
+            <Image src='/woman.png' alt='User Profile' height={24} width={24} />
+          </div>
+          <div>
+
             name, career, website
           </div>
           <div>Edit, add user icons</div>
