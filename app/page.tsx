7a9601ab88f23bd5dc9b861ff0305181bfac8ca8
catherine <vuthi.esther@gmail.com>
2024-05-22 23:00:52 +0300

    Gap changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import Image from "next/image";
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-12 gap-5">
+    <main className='flex min-h-screen flex-col items-center p-12 gap-5'>
       <h1 className='text-2xl text-violet-600 font-bold'>User Profiles</h1>
-      <div className=" flex flex-col h-auto w-full rounded-lg border border-white p-8">
-        <div className="flex">
-          <div>
-            <Image src='/woman.png' alt='User Profile' height={70} width={70} className="border-2 border-violet-600 rounded-full p-2" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <h1 className="font-bold text-lg">Jane Doe</h1>
-            <p>Software Developer</p>
-            <p>catherine.gmail.com</p>
-            <Link href='/'>https://catherine.com</Link>
+      <div className=' flex flex-col h-auto w-full rounded-lg border border-white p-8'>
+        <div className='flex justify-between'>
+          <div className='flex gap-4'>
+            <div>
+              <Image src='/woman.png' alt='User Profile' height={70} width={70} className='border-2 border-violet-600 rounded-full p-2' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h1 className='font-bold text-lg'>Jane Doe</h1>
+              <p>Software Developer</p>
+              <p>catherine.gmail.com</p>
+              <Link href='/'>https://catherine.com</Link>
+            </div>
           </div>
           <div>
             <Link href='/edit'>Edit icon</Link>
@@ -23,7 +25,10 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <p className='text-sm'>Elit voluptate dolore qui commodo est aliquip non duis magna laborum minim. Aliquip in ex minim eu amet ea qui non non voluptate. Reprehenderit duis esse excepteur quis amet velit labore deserunt do. In anim officia do ea consequat pariatur dolor. Anim commodo sunt voluptate ea nulla laboris elit mollit. Amet nostrud sit ad eiusmod. Eiusmod excepteur qui dolore nisi do deserunt officia officia qui do.</p>
+          <p className='text-sm'>
+            Elit voluptate dolore qui commodo est aliquip non duis magna laborum minim. Aliquip in ex minim eu amet ea qui non non voluptate. Reprehenderit duis esse excepteur quis amet velit labore deserunt do. In anim officia do ea consequat
+            pariatur dolor. Anim commodo sunt voluptate ea nulla laboris elit mollit. Amet nostrud sit ad eiusmod. Eiusmod excepteur qui dolore nisi do deserunt officia officia qui do.
+          </p>
         </div>
         <div>Social icons, linkedin, twitter, facebook</div>
       </div>
