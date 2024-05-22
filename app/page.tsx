95e2fdf786fb374c1afb19e5e945406cee300006
catherine <vuthi.esther@gmail.com>
2024-05-22 22:43:52 +0300

    height and padding changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12 gap-5">
       <h1>User profiles</h1>
-      <div className=" flex flex-col h-40 w-full rounded-lg border border-white">
+      <div className=" flex flex-col h-auto w-full rounded-lg border border-white p-8">
         <div className="flex">
           <div>
             <Image src='/woman.png' alt='User Profile' height={24} width={24} />
