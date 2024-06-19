58df08130cf3cd8b899295624e68c4ef3ca8b7e2
catherine <vuthi.esther@gmail.com>
2024-06-19 09:35:23 +0300

    Add login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Login from './ui/login'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center p-6 md:p-12 gap-5 text-slate-300'>
-      <h1>Home page</h1>
+    <main className='flex min-h-screen flex-col items-center justify-center p-6 md:p-12 gap-5 text-slate-300'>
+      <Login />
     </main>
   )
 }
---
+++ app/ui/login.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Login({}: Props) {
+  return (
+    <div className='w-screen flex flex-col items-center gap-10'>
+      <h1 className='text-3xl text-emerald-400 font-bold'>Login</h1>
+      <form className='flex flex-col gap-5 w-1/2 items-center'>
+        <input type='text' placeholder='Username' className='p-3 rounded-lg w-1/2' />
+        <input type='text' placeholder='Password' className='p-3 rounded-lg w-1/2' />
+        <button type='submit' className='bg-emerald-400 rounded-full px-10 py-3 font-bold'>
+          Login
+        </button>
+      </form>
+    </div>
+  )
+}
